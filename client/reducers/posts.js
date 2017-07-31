b78f504d615f78e4e93d99cc9759d3c3a4dbb7fb
Lewis Booth <lewis@ziweb.co.uk>
2017-07-31 15:54:59 +0100

    Routing & reducers set up

---
+++ client/reducers/posts.js
@@ -0,0 +1,7 @@
+// Takes in an action (what happened) and a copy of the current state
+// Returns the updated state
+
+export default function posts(state=[], action) {
+  console.log(state, action);
+  return state;
+}
