e956589b7304c00c3784feecfa167b388f3f28ac
Lewis Booth <lewis@lewisbooth.co.uk>
2017-08-09 22:00:42 +0100

    Aaaand reset for a refresher

--- client/reducers/comments.js
+++
@@ -1,4 +0,0 @@
-export default function comments(state=[], action) {
-  console.log(state, action);
-  return state;
-}
--- client/reducers/posts.js
+++
@@ -1,7 +0,0 @@
-// Takes in an action (what happened) and a copy of the current state
-// Returns the updated state
-
-export default function posts(state=[], action) {
-  console.log(state, action);
-  return state;
-}
