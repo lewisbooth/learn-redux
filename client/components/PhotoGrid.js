ee9240c17b27a51e768878a2c6a2a3bb86248aa3
Lewis Booth <lewis@lewisbooth.co.uk>
2017-08-09 22:40:21 +0100

    Started building components, ready for router

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className='photo-grid'>
+        Grid
+      </div>
+    )
+  }
+}
+
+export default Main;
